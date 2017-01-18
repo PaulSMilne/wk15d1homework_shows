8c40c5c35d2edd4800ea173294bdc3f6e8c10d20
Paul Milne <hallhill@gmail.com>
2017-01-18 14:01:21 +0000

    dropped User table in anticipation of using devise to create some authentication out of the box

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-     has_many :favorite_shows
-     has_many :favorites, through: :favorite_shows, source: :show
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117160328) do
+ActiveRecord::Schema.define(version: 20170118135906) do
 
   create_table "favorite_shows", force: :cascade do |t|
     t.integer  "show_id"
@@ -28,12 +28,6 @@ ActiveRecord::Schema.define(version: 20170117160328) do
     t.string   "programmeID"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
